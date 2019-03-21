59117614e0bb3fdf1097262b1b9feff7b2824ac5
dmytrona <dmytrona@wix.com>
2019-03-21 22:55:49 +0200

    add local search general algorithm for solving combinatorial problems

--- src/com/example/algorithms/combinatorial/HeuristicSearchMethods.scala
+++ src/com/example/algorithms/combinatorial/HeuristicSearchMethods.scala
@@ -3,14 +3,15 @@ package com.example.algorithms.combinatorial
 import com.example.algorithms.combinatorial.HeuristicSearchMethods.RandomSampling
 import com.example.algorithms.combinatorial.TravelSalesmanProblem.TspInstance
 
+import scala.annotation.tailrec
+
 object HeuristicSearchMethods {
   trait RandomSampling[T] {
-
     def makeRandomSolution(): T
 
     def solutionCost(rndSolution: T): Double
 
-    def solveWithSampling(samplesCount: Int, bestSolution: T): T = {
+    def solve(samplesCount: Int, bestSolution: T): T = {
       val (result, _) = 1.to(samplesCount).foldLeft(bestSolution -> solutionCost(bestSolution)) {
         case ((best, bestCost), _) =>
           val rndSolution = makeRandomSolution()
@@ -20,6 +21,45 @@ object HeuristicSearchMethods {
       }
 
       result
+    }
+  }
+
+  trait LocalSearch[T, P] {
+    case class SearchStep(stuck: Boolean, solution: T, cost: Double)
+
+    def makeRandomSolution(): T
+
+    def solutionCost(rndSolution: T): Double
+
+    def elements(solution: T): Seq[P]
+
+    def swapElements(solution: T, i: Int, j: Int): (Double, T) = ???
+
+    @tailrec
+    private def doSolve(searchStep: SearchStep): T = {
+      if (searchStep.stuck)
+        searchStep.solution
+      else {
+        val size = elements(searchStep.solution).size
+        val swaps: Seq[(Int, Int)] = 0.to(size).flatMap {
+          i => i.to(size).map(j => (i, j))
+        }
+        val nextStep = swaps.foldLeft(searchStep.copy(stuck = true)) {
+          case (step @ SearchStep(_, solution, cost), (i, j)) =>
+            val (delta, improvedSolution) = swapElements(solution, i, j)
+            if (delta < 0)
+              SearchStep(stuck = false, improvedSolution, cost + delta)
+            else
+              step
+        }
+
+        doSolve(nextStep)
+      }
+    }
+
+    def solve(): T = {
+      val rnd = makeRandomSolution()
+      doSolve(SearchStep(stuck = false, rnd, solutionCost(rnd)))
     }
   }
 }
@@ -35,7 +75,7 @@ object TspRandomSampled extends App {
     }
 
     override def solutionCost(rndSolution: TspInstance): Double = rndSolution.cost()
-  }.solveWithSampling(2000000, problem)
+  }.solve(2000000, problem)
 
   println("Ended up with")
   println(solution)
